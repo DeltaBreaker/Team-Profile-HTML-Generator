78fd7f37fe5c1bd6e34ab5a00015b4ba7f181edf
willc <willcrain1@gmail.com>
2022-10-25 03:20:30 -0400

    Now prompts the user for all member details until user selects to finish

--- index.js
+++ index.js
@@ -7,25 +7,42 @@ let Engineer = require("./lib/engineer.js");
 async function getUserInput() {
     let teamMembers = [];
     
-    
+    teamMembers.push(await getMemberDetails(Manager));
 
-    console.log(teamMembers);
-
-    // menu to add intern engineer or employee
+    let addNewMember = true;
+    while(addNewMember) {
+        let response = await input.prompt({
+            type: "list",
+            message: "Do you want to add another member?",
+            name: "result",
+            choices: [
+                "Engineer",
+                "Intern",
+                "No, im finished"
+            ]
+        }).then(response => { return response.result });
+        
+        if(response === "No, im finished") {
+            addNewMember = false;
+            break;
+        } else {
+            teamMembers.push(await getMemberDetails(eval(response)));
+        }
+    }
 
     generateWebpage(teamMembers);
 }
 
 async function getMemberDetails(type) {
-    let employeeType = eval(type);
-    let prompt = await input.prompt(employeeType.QUESTIONS).then(function(response) {
-        let classType = eval("employeeType." + type);
-        let value = new classType(response);
+    let prompt = await input.prompt(type.QUESTIONS).then(function(response) {
+        let value = new type.obj(response);
         return value;
     });
     return prompt;
 }
 
 function generateWebpage(members) {
-
+    console.log(teamMembers);
 }
+
+getUserInput();
